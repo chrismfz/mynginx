e8e4d0d787b0f843792a3a73b6d4e2e7a34e3e00
Chris <chrismfz@gmail.com>
2025-12-17 02:13:34 +0200

    Add deployment script for NGINX with HTTP/3 support
    This script automates the deployment of NGINX with HTTP/3 and Brotli support, including SSL setup and firewall configuration.

--- deploy_quic.sh
+++ deploy_quic.sh
@@ -1,1 +1,139 @@
 
+#!/bin/bash
+set -e
+
+# --- CONFIGURATION ---
+DOMAIN="quic.myip.gr"
+EMAIL="admin@$DOMAIN"
+MASTER_CONF_URL="https://raw.githubusercontent.com/chrismfz/mynginx/refs/heads/main/nginx.conf.master"
+
+# Paths
+SRC_DIR="/usr/local/src"
+NGINX_PATH="/opt/nginx"
+AWS_LC_PATH="/opt/aws-lc"
+HTML_PATH="/opt/nginx/html"
+
+echo "--- 1. Installing Build Dependencies & Tools ---"
+apt update
+apt install -y build-essential cmake git curl wget perl golang-go \
+               libpcre3-dev zlib1g-dev libpcre2-dev libssl-dev certbot ufw
+
+echo "--- 2. Fetching Source Code & Master Config ---"
+mkdir -p $SRC_DIR
+mkdir -p $NGINX_PATH/conf
+mkdir -p $HTML_PATH
+
+# Clone or Update Repos
+declare -A REPOS=(
+    ["aws-lc"]="https://github.com/aws/aws-lc.git"
+    ["ngx_brotli"]="https://github.com/google/ngx_brotli.git"
+    ["nginx"]="https://github.com/nginx/nginx.git"
+)
+
+for repo in "${!REPOS[@]}"; do
+    cd $SRC_DIR
+    if [ -d "$repo" ]; then
+        echo "Updating $repo..."
+        cd "$repo" && git pull && git submodule update --init && cd ..
+    else
+        echo "Cloning $repo..."
+        git clone --recursive "${REPOS[$repo]}"
+    fi
+done
+
+# Download your Master Config from GitHub
+echo "Downloading master config template..."
+curl -sL $MASTER_CONF_URL -o $NGINX_PATH/conf/nginx.conf.master
+
+echo "--- 3. Compiling AWS-LC (The Crypto Engine) ---"
+cd $SRC_DIR/aws-lc
+rm -rf build && mkdir build && cd build
+cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$AWS_LC_PATH -DBUILD_SHARED_LIBS=OFF
+cmake --build . --target install
+
+echo "--- 4. Compiling NGINX with HTTP/3 & Brotli ---"
+cd $SRC_DIR/nginx
+./auto/configure \
+    --prefix=$NGINX_PATH \
+    --with-debug \
+    --with-http_ssl_module \
+    --with-http_v2_module \
+    --with-http_v3_module \
+    --with-cc-opt="-I$AWS_LC_PATH/include" \
+    --with-ld-opt="-L$AWS_LC_PATH/lib -lssl -lcrypto -lstdc++" \
+    --with-pcre-jit \
+    --with-threads \
+    --with-file-aio \
+    --with-http_gzip_static_module \
+    --add-module=$SRC_DIR/ngx_brotli
+
+make -j$(nproc)
+make install
+
+echo "--- 5. System Integration (Systemd Unit) ---"
+cat <<EOF > /etc/systemd/system/nginx.service
+[Unit]
+Description=The NGINX HTTP and reverse proxy server (Custom Build)
+After=network-online.target
+Wants=network-online.target
+
+[Service]
+Type=forking
+PIDFile=$NGINX_PATH/logs/nginx.pid
+ExecStartPre=$NGINX_PATH/sbin/nginx -t
+ExecStart=$NGINX_PATH/sbin/nginx
+ExecReload=$NGINX_PATH/sbin/nginx -s reload
+ExecStop=/bin/kill -s QUIT \$MAINPID
+PrivateTmp=true
+AmbientCapabilities=CAP_NET_BIND_SERVICE
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+systemctl daemon-reload
+
+echo "--- 6. Initial Config Deployment (Pre-SSL) ---"
+# Generate the live config from the master template
+cp $NGINX_PATH/conf/nginx.conf.master $NGINX_PATH/conf/nginx.conf
+sed -i "s/{{DOMAIN}}/$DOMAIN/g" $NGINX_PATH/conf/nginx.conf
+
+# Start NGINX so Certbot can perform the webroot challenge
+systemctl enable --now nginx
+
+echo "--- 7. SSL Setup (Webroot Mode - Zero Downtime) ---"
+# Request ECDSA cert using the existing HTML directory for the ACME challenge
+certbot certonly --webroot \
+    -w $HTML_PATH \
+    --non-interactive --agree-tos --email "$EMAIL" \
+    --key-type ecdsa \
+    -d "$DOMAIN" \
+    --deploy-hook "$NGINX_PATH/sbin/nginx -s reload"
+
+echo "--- 8. Security: Firewall (UFW) ---"
+ufw allow ssh
+ufw allow 80/tcp
+ufw allow 443/tcp
+ufw allow 443/udp
+ufw --force enable
+
+echo "--- 9. Setting Up Logrotate ---"
+cat <<EOF > /etc/logrotate.d/nginx-custom
+$NGINX_PATH/logs/*.log {
+    daily
+    rotate 14
+    compress
+    notifempty
+    create 0640 root root
+    sharedscripts
+    postrotate
+        [ -f $NGINX_PATH/logs/nginx.pid ] && kill -USR1 \$(cat $NGINX_PATH/logs/nginx.pid)
+    endscript
+}
+EOF
+
+# Final reload to ensure SSL is picked up
+$NGINX_PATH/sbin/nginx -s reload
+
+echo "--- DEPLOYMENT COMPLETE ---"
+echo "Test your HTTP/3 here: https://http3check.net/?host=$DOMAIN"
